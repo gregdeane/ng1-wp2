dd4e1e2f7b4b6a7c2b48022950a9e5fff59fce95
Greg Deane <greg.deane.dev@gmail.com>
2016-11-03 21:00:31 +0100

    chore(tools): add prompt wip

--- tools/start.ts
+++ tools/start.ts
@@ -1,20 +1,41 @@
 import checkRequiredFiles from './utils/checkRequiredFiles';
-// import detect from 'detect-port';
-// import chalk from 'chalk';
-// import prompt from 'react-dev-utils/prompt.js';
+import * as detect from 'detect-port';
+import * as chalk from 'chalk';
+import prompt from './utils/prompt';
 import clearConsole from './utils/clearConsole';
 // import runDevServer from './server';
 import paths from '../config/paths-ts';
 
-// const DEFAULT_PORT = process.env.PORT || 3000;
+const DEFAULT_PORT = process.env.PORT || 8080;
 
 // warn and crash if required files are missing
 if (!checkRequiredFiles([paths.indexHtml, paths.indexTs])) {
   process.exit(1);
 }
 
-console.log('========= start ===========');
-// console.log('checkRequiredFiles', checkRequiredFiles);
-// console.log('paths', paths);
-// clearConsole();
-console.log('========= end ===========');
+const promptUser = (port: number) => {
+  let question = [
+    chalk.yellow(`Something is already running on port ${DEFAULT_PORT}.\n\n`),
+    'Would you like to run the app on another port instead?'
+  ].join('');
+
+  clearConsole();
+
+  prompt(question, true).then(shouldChangePort => {
+    // if (shouldChangePort) {
+    //   run(port);
+    // }
+  });
+};
+
+// attempt to use the default port. if it's in use, offer to run on
+// a different port. `detect()` Promise resolves to the next free port.
+detect(DEFAULT_PORT).then((port: number) => {
+  if (port === DEFAULT_PORT) {
+    // run(port);
+    return;
+  }
+
+  promptUser(port);
+
+});
